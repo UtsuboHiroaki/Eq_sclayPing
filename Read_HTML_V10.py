34bb7081fdb7ec7d9e729581a9f2397ea79dfe28
UtsuboHiroaki <hutsubo@gmail.com>
2023-01-09 18:13:27 +0900

    if __name__ == "__main__":の設定

--- Read_HTML_V10.py
+++ Read_HTML_V10.py
@@ -172,19 +172,20 @@ def create_csv(title, cash_flow_list):
         writer.writeheader()
         writer.writerows(cash_flow_list)
 
-find_word1 = '連結キャッシュ・フロー計算書'
-find_words = ['営業活動によるキャッシュ・フロー', '減価償却費', '固定資産']
-find_words2 = ['支出', '収入']
-bath_path = Path(__file__).resolve().parent
-cash_flow_list = []
-sec_codes = []
-csv_file_path = bath_path / 'screening' / 'csv_differ.csv'
-csv_open(csv_file_path)
-print(sec_codes)
-del_folder(bath_path)
-sec_code_reserch(sec_codes, bath_path, find_word1, find_words, find_words2)
-del_folder(bath_path)
-pprint.pprint(cash_flow_list)
-title = 'CASAFLOWDATA_'
-create_csv(title, cash_flow_list)
-print('done!')
+if __name__ == "__main__":
+    find_word1 = '連結キャッシュ・フロー計算書'
+    find_words = ['営業活動によるキャッシュ・フロー', '減価償却費', '固定資産']
+    find_words2 = ['支出', '収入']
+    bath_path = Path(__file__).resolve().parent
+    cash_flow_list = []
+    sec_codes = []
+    csv_file_path = bath_path / 'screening' / 'csv_differ.csv'
+    csv_open(csv_file_path)
+    print(sec_codes)
+    del_folder(bath_path)
+    sec_code_reserch(sec_codes, bath_path, find_word1, find_words, find_words2)
+    del_folder(bath_path)
+    pprint.pprint(cash_flow_list)
+    title = 'CASAFLOWDATA_'
+    create_csv(title, cash_flow_list)
+    print('done!')
